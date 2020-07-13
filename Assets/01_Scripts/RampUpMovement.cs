f7a9c0d003648fc1126bc8b65fdfc9d5861cd172
Joss <tobar.josseline@gmail.com>
2020-07-12 22:57:21 -0400

    Spawners test

---
+++ Assets/01_Scripts/RampUpMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampUpMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector2 startPosition;
+
+    
+    void Start()
+    {
+        startPosition = transform.position;
+        speed = .5f;
+    }
+    void FixedUpdate()
+    {
+
+        //transform.position = new Vector2(startPosition.x, startPosition.y * movSpeed); 
+        //transform.position = new Vector2(startPosition.x + Mathf.Sin(Time.time * speed), transform.position.y);
+        Move();
+        Destroy();
+    }
+
+    private void Move()
+    {
+
+        transform.position = new Vector2(startPosition.x, transform.position.y + Time.deltaTime * speed);
+    }
+
+    private void Destroy()
+    {
+        Vector2 ScreenBoundarie = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        if (transform.position.y >= ScreenBoundarie.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/01_Scripts/SpawnObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private GameObject Platform;
+    [SerializeField] private float TimeBetweenSpawns;
+   
+    Vector2 SpawnerPosition;
+    void Start()
+    {
+        
+        SpawnerPosition = new Vector2(transform.position.x, transform.position.y);
+        
+        
+        StartCoroutine(Spawning());
+    }
+
+   
+
+    private IEnumerator Spawning()
+    {
+        while (true) {
+            
+            GameObject A = (GameObject)Instantiate(Platform);
+            A.transform.parent = transform;
+            A.transform.position = SpawnerPosition;
+            TimeBetweenSpawns = Random.Range(1, 10);
+            yield return new WaitForSeconds(TimeBetweenSpawns);
+       
+
+        }
+
+    }
+}
