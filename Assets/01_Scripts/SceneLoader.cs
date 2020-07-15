6a4e7cb4b3ab21325f620bcb1395a6a5a6a4dcb6
Joss <tobar.josseline@gmail.com>
2020-07-14 21:22:40 -0400

    added 'SceneLoader' script & 'GameManager' gameobject to the MainMenu scene

---
+++ Assets/01_Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    
+    public int getLocalSceneIndex;
+
+    void Start()
+    {
+        getLocalSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(getLocalSceneIndex + 1);
+    
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
