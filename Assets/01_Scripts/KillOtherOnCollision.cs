5453ceb1e7b16e5eefe12384fc07c9d43420248d
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-15 23:00:09 -0600

    :recycle: Updating script.

--- Assets/01_Scripts/KillOtherOnCollision.cs
+++ Assets/01_Scripts/KillOtherOnCollision.cs
@@ -6,6 +6,16 @@ public class KillOtherOnCollision : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
-        other.gameObject.SetActive(false);
+        Deactivate(other.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Deactivate(other.gameObject);
+    }
+
+    private void Deactivate(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
     }
 }
