d55734d005128c198babbbecc17099a53bcf770e
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-16 23:59:44 -0600

    :recycle: Moving common strings to a single class.

---
+++ Assets/01_Scripts/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+     //Input
+    public const string HorizontalAxis = "Horizontal";
+
+    //Tags
+    public const string TagLife = "Life";
+}
