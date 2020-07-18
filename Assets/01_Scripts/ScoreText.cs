acdb206ecd4be7cff8fa7cbff1e7e000eaa27eb6
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-18 17:02:31 -0600

    :sparkles: Adding script to handle score updating.

---
+++ Assets/01_Scripts/ScoreText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreText : MonoBehaviour
+{
+
+    private GameInfo gameInfo;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        gameInfo = FindObjectOfType<GameInfo>();
+    }
+
+    void Update()
+    {
+        scoreText.text = gameInfo.GetScore().ToString();
+    }
+}
