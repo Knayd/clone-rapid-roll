8f4a9428780653af01833c77787fad8ad84e6f3e
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-21 01:17:32 -0600

    :sparkles: Adding base class to spawn objects.

---
+++ Assets/01_Scripts/BaseObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseObjectSpawner : MonoBehaviour
+{
+    public abstract GameObject GetObjectToSpawn();
+    public abstract float GetTimeBetweenSpawns();
+    public abstract SpawnArea GetObjectSpawnArea();
+    public abstract string GetObjectTagOnWhichToSpawn();
+
+    public IEnumerator SpawnObject()
+    {
+        while (true)
+        {
+            var objectOnWhichToSpawn = GetObjectOnWichToSpawn();
+            if (objectOnWhichToSpawn != null)
+            {
+                if (!GetObjectToSpawn().activeInHierarchy)
+                {
+                    GetObjectToSpawn().transform.position = objectOnWhichToSpawn.transform.position;
+                    GetObjectToSpawn().SetActive(true);
+                    yield break;
+                }
+            }
+            yield return new WaitForSeconds(GetTimeBetweenSpawns());
+        }
+    }
+
+    private GameObject GetObjectOnWichToSpawn()
+    {
+        return GetObjectSpawnArea()
+                .GetObjectsInsideArea()
+                .Find(objectInArea => objectInArea.CompareTag(GetObjectTagOnWhichToSpawn()));
+    }
+}
