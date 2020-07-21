88c392191689cdc4dd973cb980c1ce18fff5e59b
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-21 01:17:06 -0600

    :sparkles: Adding spawn area script.

---
+++ Assets/01_Scripts/SpawnArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpawnArea : MonoBehaviour
+{
+    private List<GameObject> objectsInsideArea;
+    public List<GameObject> GetObjectsInsideArea()
+    {
+        return objectsInsideArea;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!objectsInsideArea.Contains(other.gameObject))
+        {
+            objectsInsideArea.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        objectsInsideArea.Remove(other.gameObject);
+    }
+}
