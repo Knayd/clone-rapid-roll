26155fe636a6a9c3beb7c9730c99c950301168f3
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-21 23:34:52 -0600

    :sparkles: Adding player spawner.

---
+++ Assets/01_Scripts/PlayerSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : BaseObjectSpawner
+{
+    [SerializeField] SpawnArea spawnArea;
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        StartCoroutine(SpawnObject());
+    }
+
+    public override SpawnArea GetObjectSpawnArea()
+    {
+        return spawnArea;
+    }
+
+    public override GameObject GetObjectToSpawn()
+    {
+        return player;
+    }
+
+    public override string GetObjectTagOnWhichToSpawn()
+    {
+        return Constants.TagPlatform;
+    }
+
+    public override float GetTimeBetweenSpawns()
+    {
+        return 1f;
+    }
+}
