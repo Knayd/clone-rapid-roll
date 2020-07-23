d80e9b6b619370562bde555f07feab960c0a9dd9
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-22 23:19:19 -0600

    :sparkles: Adding lives spawner.

---
+++ Assets/01_Scripts/LivesSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesSpawner : ObjectSpawnerOnTopOfPlatform
+{
+
+    [SerializeField] SpawnArea spawnArea;
+    [SerializeField] GameObject lifeObject;
+    private float maxTimeBetweenSpawns = 1f;
+    private float minTimeBetweenSpawns = 4f;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            // Spawn object indefinitely
+            yield return StartCoroutine(SpawnObject());
+        }
+    }
+
+    public override SpawnArea GetObjectSpawnArea()
+    {
+        return spawnArea;
+    }
+
+    public override GameObject GetObjectToSpawn()
+    {
+        return lifeObject;
+    }
+
+    public override float GetTimeInSecondsBetweenSpawns()
+    {
+        return Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+    }
+
+    public override Vector2 ObjectToSpawnHalfHeight()
+    {
+        var lifeBounds = lifeObject.GetComponent<SpriteRenderer>().bounds;
+        return new Vector2(0, lifeBounds.extents.y);
+    }
+}
