ce108149207c26fd31816cc16afc3c247cb5c098
Deryan Argueta <deryan.hndz@gmail.com>
2020-07-22 22:18:10 -0600

    :recycle: Updating parent class.

--- Assets/01_Scripts/PlayerSpawner.cs
+++ Assets/01_Scripts/PlayerSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawner : BaseObjectSpawner
+public class PlayerSpawner : ObjectSpawnerOnTopOfPlatform
 {
     [SerializeField] SpawnArea spawnArea;
     [SerializeField] GameObject player;
