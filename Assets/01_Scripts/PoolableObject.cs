8f19d73923485dea80cbaaf51b0d45ebebcbc52a
Deryan Argueta <deryan.hndz@gmail.com>
2020-08-02 16:23:31 -0600

    ♻️ Adding PoolableObject script.

---
+++ Assets/01_Scripts/PoolableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Poolable Object")]
+public class PoolableObject : ScriptableObject
+{
+    [SerializeField] private GameObject objectToPool;
+    [SerializeField] private int amountToPool;
+
+    public GameObject GetObjectToPool() { return objectToPool; }
+
+    public int GetAmountToPool() { return amountToPool; }
+}
