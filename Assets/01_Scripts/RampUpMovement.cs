e592ed6ae2f7bcdfe18fd64043f0c346e85506ef
Deryan Argueta <deryan.hndz@gmail.com>
2020-08-04 00:25:01 -0600

    🔥 Removing unnecessary code.

--- Assets/01_Scripts/RampUpMovement.cs
+++ Assets/01_Scripts/RampUpMovement.cs
@@ -7,7 +7,6 @@ public class RampUpMovement : MonoBehaviour
     [SerializeField] float speed;
     Vector2 startPosition;
 
-    
     void Start()
     {
         startPosition = transform.position;
@@ -15,25 +14,11 @@ public class RampUpMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-
-        //transform.position = new Vector2(startPosition.x, startPosition.y * movSpeed); 
-        //transform.position = new Vector2(startPosition.x + Mathf.Sin(Time.time * speed), transform.position.y);
         Move();
-        Destroy();
     }
 
     private void Move()
     {
-
         transform.position = new Vector2(startPosition.x, transform.position.y + Time.deltaTime * speed);
-    }
-
-    private void Destroy()
-    {
-        Vector2 ScreenBoundarie = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-        if (transform.position.y >= ScreenBoundarie.y)
-        {
-            Destroy(gameObject);
-        }
     }
 }
