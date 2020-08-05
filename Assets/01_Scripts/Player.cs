999ae8c0b5635879c1778be4e29723f99269fe77
Deryan Argueta <deryan.hndz@gmail.com>
2020-08-04 21:57:11 -0600

    ♻️ Changing invocation order.

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -57,8 +57,8 @@ public class Player : MonoBehaviour
     {
         if (spikes.CompareTag(Constants.TagSpike))
         {
-            levelManager.DecreaseLives();
             gameObject.SetActive(false);
+            levelManager.DecreaseLives();
         }
     }
 
