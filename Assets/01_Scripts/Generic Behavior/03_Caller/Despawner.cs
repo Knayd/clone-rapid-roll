b8705b791452f591f8da6549c235b187dacf66fd
Deryan Argueta <deryan.hndz@gmail.com>
2020-08-23 18:07:43 -0600

    ✨ Adding generic caller for despawnable objects.

---
+++ Assets/01_Scripts/Generic Behavior/03_Caller/Despawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Despawner : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        IDespawnable objectToDespawn = other.gameObject.GetComponent<IDespawnable>();
+        if (objectToDespawn != null)
+        {
+            objectToDespawn.Despawn();
+        }
+    }
+}
