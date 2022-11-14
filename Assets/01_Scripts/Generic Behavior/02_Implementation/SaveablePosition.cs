62dbf9d4470de20f954a60721a2118744bae5b9e
Deryan Argueta <deryan.hndz@gmail.com>
2022-11-13 23:32:48 -0600

    Adding script to save object position

---
+++ Assets/01_Scripts/Generic Behavior/02_Implementation/SaveablePosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveablePosition : MonoBehaviour, ISaveable
+{
+    public object CaptureState()
+    {
+        var position = transform.position;
+        return new Position
+        {
+            x = position.x,
+            y = position.y
+        };
+    }
+
+    public void RestoreState(object state)
+    {
+        var position = (Position)state;
+        transform.position = new Vector3(position.x, position.y);
+    }
+
+    [Serializable]
+    private struct Position
+    {
+        public float x;
+        public float y;
+    }
+}
