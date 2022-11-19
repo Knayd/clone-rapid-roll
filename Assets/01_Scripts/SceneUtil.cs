fba587035d5da5f19368808db3970ae18e340316
Deryan Argueta <deryan.hndz@gmail.com>
2022-11-19 17:27:28 -0600

    Adding method to get all objects in scene

---
+++ Assets/01_Scripts/SceneUtil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+
+public static class SceneUtil
+{
+    public static List<T> GetAllObjectsInScene<T>()
+    {
+        List<GameObject> objectsInScene = new List<GameObject>();
+
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (!EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
+                objectsInScene.Add(go);
+        }
+
+        var objects = objectsInScene.Select(gameObject => gameObject.GetComponent<T>()).Where(component => component != null).ToList();
+
+        return objects;
+    }
+}
